be4636b0213ad1664344a352487f9fbd89e6dda1
Jongkeun Choi <jongkeun.ch@gmail.com>
2025-01-02 00:02:46 +0900

    레이아웃 통합

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import expressLayouts from "express-ejs-layouts";
 import "dotenv/config";
 
 const app = express();
@@ -8,7 +9,16 @@ const app = express();
 app.use(express.static(path.join(__dirname, "..", "/public")));
 app.set("views", path.join(__dirname, "..", "/views"));
 app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layout"); // 'views/layout.ejs' 파일을 레이아웃으로 사용
+
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "",
+  });
+});
 
 app.get("/test", (req, res) => {
   res.send(`Express on Vercel: ${process.env.HELLO}`);
@@ -16,7 +26,7 @@ app.get("/test", (req, res) => {
 
 app.get("/form", (req, res) => {
   const userName = req.query.name || "손님";
-  res.render("form", { userName });
+  res.render("form", { title: "폼 전송", userName });
 });
 
 app.post("/form", (req, res) => {
@@ -27,6 +37,7 @@ app.post("/form", (req, res) => {
   const userPasswordConfirm = req.body.userPasswordConfirm;
   const userEmail = req.body.userEmail;
   res.render("form_result", {
+    title: "폼 전송 결과",
     userName,
     userPassword,
     userPasswordConfirm,
@@ -35,11 +46,13 @@ app.post("/form", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", {
+    title: "ᗜ•   ̯ •̥ ᗜ",
+  });
 });
 
 app.listen(3000, () => {
-  console.log("Server ready on port 3000.");
+  console.log("서버 실행 중: http://localhost:3000");
 });
 
 export default app;
