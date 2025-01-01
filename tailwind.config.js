fcc20ce3f87e90aa176fc7c06a2e88e349ee0297
Jongkeun Choi <jongkeun.ch@gmail.com>
2025-01-01 23:39:23 +0900

    tailwindcss 통합 & 웹폰트 적용

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./api/**/*.{ts,js}", "./views/**/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
